6712dd400526f6a59d41443f6701c022fc63f89f
DoDoBest <send1684@gmail.com>
2024-03-12 17:19:56 +0900

    fix: 종료, 뒤로가기 옵션 출력하도록 수정

--- src/main/kotlin/view/Screen.kt
+++ src/main/kotlin/view/Screen.kt
@@ -18,11 +18,28 @@ sealed class Screen: KioskContract.View {
         for ((index, food) in foods.withIndex()) {
             println("${index+1}. ${food.name.padEnd(trailingSpace)}|${convertPrice(food)}${food.description}")
         }
+        showExitOption(trailingSpace)
         println()
+    }
+
+    fun waitInput() {
+        val input = readlnOrNull() ?: run {
+            backToPreviousScreen()
+            return
+        }
+        presenter.checkInput(input)
     }
 
     open fun backToPreviousScreen() {
         ScreenStack.pop()
+    }
+
+    private fun showExitOption(trailingSpace: Int) {
+        if (category == ScreenCategory.HOME) {
+            println("0. ${"종료".padEnd(trailingSpace)}| 프로그램 종료")
+            return
+        }
+        println("0. ${"뒤로가기".padEnd(trailingSpace)}| 뒤로가기")
     }
 
     private fun showTitle(category: ScreenCategory) {
